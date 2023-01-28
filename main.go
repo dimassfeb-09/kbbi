938763b88b86820fe410590ea4a5ca1fd0ca07eb
DIMAS FEBRIYANTO <dimasfebriyanto@MacBook-Pro-DIMAS.local>
2023-01-28 10:42:45 +0700

    update  main.go

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
-package kbbi
+package main
+
+import (
+	"fmt"
+	kbbi "github.com/dimassfeb-09/kbbi.git/kbbi"
+	"log"
+)
+
+func main() {
+	if result, err := kbbi.Search("baju"); err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+}
