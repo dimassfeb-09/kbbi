035b6a5d68cac3a0ba8d9826c40f924bb59a291d
DIMAS FEBRIYANTO <dimasfebriyanto@MacBook-Pro-DIMAS.local>
2023-01-28 10:05:33 +0700

    Fetch data

--- kbbi/fetch_data.go
+++ kbbi/fetch_data.go
@@ -2,19 +2,15 @@ package kbbi
 
 import (
 	"encoding/json"
+	"github.com/dimassfeb-09/kbbi.git/entity"
 	"io"
 	"log"
 	"net/http"
 )
 
-type ResponseKBBI struct {
-	Lema string   `json:"lema"`
-	Arti []string `json:"arti"`
-}
+func Search(keyword string) (*entity.ResponseKBBI, error) {
 
-func Search(keyword string) (*ResponseKBBI, error) {
-
-	var responseKBBI *ResponseKBBI
+	var responseKBBI *entity.ResponseKBBI
 
 	response, err := http.Get("https://kbbi-api-zhirrr.vercel.app/api/kbbi?text=" + keyword)
 	if err != nil {
