d76f12e3409d159840c85bc9eef2b11fb770d1df
DIMAS FEBRIYANTO <dimasfebriyanto@MacBook-Pro-DIMAS.local>
2023-01-28 10:42:23 +0700

    kbbi fetch data

--- kbbi/fetch_data.go
+++ kbbi/fetch_data.go
@@ -2,9 +2,9 @@ package kbbi
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/dimassfeb-09/kbbi.git/entity"
 	"io"
-	"log"
 	"net/http"
 )
 
@@ -14,17 +14,22 @@ func Search(keyword string) (*entity.ResponseKBBI, error) {
 
 	response, err := http.Get("https://kbbi-api-zhirrr.vercel.app/api/kbbi?text=" + keyword)
 	if err != nil {
-		log.Print(err)
+		return nil, err
 	} else {
-		bytesResponse, err := io.ReadAll(response.Body)
-		if err != nil {
-			return nil, err
-		} else {
-			err := json.Unmarshal(bytesResponse, &responseKBBI)
+		if response.StatusCode == 200 {
+			bytesResponse, err := io.ReadAll(response.Body)
 			if err != nil {
 				return nil, err
+			} else {
+				err := json.Unmarshal(bytesResponse, &responseKBBI)
+				if err != nil {
+					return nil, err
+				}
 			}
+		} else if response.StatusCode == http.StatusGatewayTimeout {
+			return nil, errors.New(keyword + " not found.")
 		}
 	}
+
 	return responseKBBI, nil
 }
